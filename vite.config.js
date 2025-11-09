88ad6c3269b2baa3691fb5b917140565fb1b3015
im-binary <pongpong0121@kakao.com>
2025-11-09 16:46:39 +0900

    fix: vite config 에 base 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  base: "/front_7th_chapter2-1",
+
   test: {
     globals: true,
-    base: "/front_7th_chapter2-1",
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
     exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
